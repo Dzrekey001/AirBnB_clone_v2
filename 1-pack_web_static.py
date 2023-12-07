f1f6de8f6e70dfbcb84aef0aff990a4564811194
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-12-07 21:06:16 +0000

    edited code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,14 +10,13 @@ from datetime import datetime
 
 def do_pack():
     """ A script that generates archive the contents of web_static folder"""
-    dir_name = "versions"
     now = datetime.now()
-    timestamp = now.strftime("%Y%m%d%H%M%S")
+    time = now.strftime("%Y%m%d%H%M%S")
 
     try:
         local("mkdir -p versions")
-        local("tar -cvzf {}/{} web_static/".format(dir_name, timestamp))
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(time))
 
-        return "{}/web_static_{}.tgz".format(dir_name, timestamp)
+        return "versions/web_static_{}.tgz".format(time)
     except Exception as e:
         return None
