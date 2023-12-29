13ce259a6f5971dcaeed4779196d92ba02db7deb
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-12-29 12:08:56 +0000

    Initial Commit

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Importing Flask to run the web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = FLask(__name__)
+
+
+@app.teardown_appcontext
+def close_session():
+    storage.close()
+
+
+@app.route('/states_list', strick_slashes=True)
+def state_ls():
+    states = storage.all()
+    return render_templates('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
